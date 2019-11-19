5dacf5fa991e115c3792d6e469ad3959b5a731cd
DENIS WHELAN - Student <G00333347@gmit.ie>
2019-11-19 13:26:51 +0000

    Update

--- Esp32/ESP_CAN_SEND/ESP_CAN_SEND.ino
+++ Esp32/ESP_CAN_SEND/ESP_CAN_SEND.ino
@@ -6,12 +6,14 @@ CAN_device_t CAN_cfg;
 void setup() {
     Serial.begin(115200);
     Serial.println("iotsharing.com CAN demo");
+    Serial.print("Enter what youd like to store and press enter: ");
     CAN_cfg.speed=CAN_SPEED_1000KBPS;
     CAN_cfg.tx_pin_id = GPIO_NUM_5;
     CAN_cfg.rx_pin_id = GPIO_NUM_4;
     CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
     //start CAN Module
     ESP32Can.CANInit();
+    
 }
 
 void loop() {
@@ -37,17 +39,24 @@ void loop() {
     }
     else
     {
-      rx_frame.FIR.B.FF = CAN_frame_std;
-      rx_frame.MsgID = 1;
-      rx_frame.FIR.B.DLC = 8;
-      rx_frame.data.u8[0] = 'h';
-      rx_frame.data.u8[1] = 'e';
-      rx_frame.data.u8[2] = 'l';
-      rx_frame.data.u8[3] = 'l';
-      rx_frame.data.u8[4] = 'o';
-      rx_frame.data.u8[5] = 'c';
-      rx_frame.data.u8[6] = 'a';
-      rx_frame.data.u8[7] = 'n';
+      if(Serial.available()>0){
+        String incomingstring = Serial.readString();
+        for(int i = 0; i<sizeof(incomingstring)-1;i++){
+          
+        }
+      }
+      
+//      rx_frame.FIR.B.FF = CAN_frame_std;
+//      rx_frame.MsgID = 1;
+//      rx_frame.FIR.B.DLC = 8;
+//      rx_frame.data.u8[0] = 'h';
+//      rx_frame.data.u8[1] = 'e';
+//      rx_frame.data.u8[2] = 'l';
+//      rx_frame.data.u8[3] = 'l';
+//      rx_frame.data.u8[4] = 'o';
+//      rx_frame.data.u8[5] = 'c';
+//      rx_frame.data.u8[6] = 'a';
+//      rx_frame.data.u8[7] = 'n';
 
       printf("Sending hellocan \n");
       ESP32Can.CANWriteFrame(&rx_frame);
