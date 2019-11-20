07f909a774f7008d7533d842c25c9e611e71405f
dwhelan97 <djwhelan15@gmail.com>
2019-11-20 20:34:51 +0000

    Playing around with RTOS on ESP32

--- Esp32/ESP_CAN_RECEIVE_AND_SEND_BACK/esp32-can-protocol-demo-master/examples/esp32can/esp32can.ino
+++ Esp32/ESP_CAN_RECEIVE_AND_SEND_BACK/esp32-can-protocol-demo-master/examples/esp32can/esp32can.ino
@@ -2,30 +2,23 @@
 #include <CAN_config.h>
 
 CAN_device_t CAN_cfg;
-int incomingByte = 0; 
 
 void setup() {
     Serial.begin(115200);
-
-    Serial.println("Enter What you want to be sent:");
+    Serial.println("iotsharing.com CAN demo");
     CAN_cfg.speed=CAN_SPEED_1000KBPS;
     CAN_cfg.tx_pin_id = GPIO_NUM_5;
     CAN_cfg.rx_pin_id = GPIO_NUM_4;
-    CAN_cfg.rx_queue = xQueueCreate(sizeof(CAN_frame_t),sizeof(CAN_frame_t));
+    CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
     //start CAN Module
     ESP32Can.CANInit();
-    
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-incomingByte = Serial.read(); // read the incoming byte:
-Serial.print(" I received:");
-Serial.println(incomingByte);
-}
     CAN_frame_t rx_frame;
     //receive next CAN frame from queue
     if(xQueueReceive(CAN_cfg.rx_queue,&rx_frame, 3*portTICK_PERIOD_MS)==pdTRUE){
+
       //do stuff!
       if(rx_frame.FIR.B.FF==CAN_frame_std)
         printf("New standard frame");
@@ -37,15 +30,13 @@ Serial.println(incomingByte);
       else{
         printf(" from 0x%08x, DLC %d\n",rx_frame.MsgID,  rx_frame.FIR.B.DLC);
         for(int i = 0; i < 8; i++){
-          printf("%c", (char)rx_frame.data.u8[i]);
+          printf("%c\t", (char)rx_frame.data.u8[i]);
         }
         printf("\n");
       }
     }
     else
     {
-      
-      rx_frame.data.u8;
       rx_frame.FIR.B.FF = CAN_frame_std;
       rx_frame.MsgID = 1;
       rx_frame.FIR.B.DLC = 8;
@@ -58,10 +49,7 @@ Serial.println(incomingByte);
       rx_frame.data.u8[6] = 'a';
       rx_frame.data.u8[7] = 'n';
 
-      //rx_frame.data = {'}
-      printf("Sending hellocan \n");
-      ESP32Can.CANWriteFrame(&rx_frame);
       
+      ESP32Can.CANWriteFrame(&rx_frame);
     }
-    delay(1000);
 }
